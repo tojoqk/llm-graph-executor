cf9bdc92171a56dec021d9226e6dcc23ee22a008
Masaya Tojo <masaya@tojo.tokyo>
2026-06-29 00:18:50 +0900

    Add typed-racket-more to deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "llm-graph-executor")
-(define deps '("base" "typed-racket-lib" "https://github.com/tojoqk/graph-executor.git"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more" "https://github.com/tojoqk/graph-executor.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/llm-graph-executor.scrbl" ())))
 (define pkg-desc "An LLM plugin for graph-executor")
