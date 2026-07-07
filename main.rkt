3a019efe60abe34e45af3419890edf399cc77224
Masaya Tojo <masaya@tojo.tokyo>
2026-07-07 23:24:43 +0900

    Update exports in main.rkt

--- main.rkt
+++ main.rkt
@@ -1,11 +1,11 @@
 #lang racket/base
 
-(require "graph/llm.rkt"
-         "prompt/llm.rkt"
+(require "llm.rkt"
+         "graph/llm.rkt"
          "executor/repl-llm.rkt"
          "history/llm.rkt")
 
-(provide (all-from-out "prompt/llm.rkt")
+(provide (all-from-out "llm.rkt")
          (all-from-out "executor/repl-llm.rkt")
          (all-from-out "history/llm.rkt")
          (all-from-out "graph/llm.rkt"))
